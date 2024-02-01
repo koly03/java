35d6921ec5ecdb77b40241e594e3ea241715590f
koly03 <kol.y0363@gmail.com>
2024-02-01 20:45:49 +0100

    It remains to add random

--- Main.java
+++ Main.java
@@ -4,11 +4,14 @@ import java.util.LinkedList;
 public class Main {
   public static void main(String[] args) {
     LinkedList<Integer> list = new LinkedList<>();
-    list.add(3);
-    list.add(5);
-    list.add(1);
-    list.add(8);
-    list.add(4);
+    // for (int i = 0; i < 10; i++) {
+    //   list.add(null);
+    // }
+    // list.add(3);
+    // list.add(5);
+    // list.add(1);
+    // list.add(8);
+    // list.add(4);
     Fusion list1 = new Fusion(list);
     System.out.println(list);
     System.out.println();
@@ -18,7 +21,7 @@ public class Main {
     // System.out.println("Before sorting - " + list1);
     // list1.sorting();
     // System.out.println("After sotring - " + list1);
-    ;
+    
 
   }
 }

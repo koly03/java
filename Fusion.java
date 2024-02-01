69067f80f03a5c71de72d2c2ce6caac8f70accd4
koly03 <kol.y0363@gmail.com>
2024-02-01 01:32:02 +0100

    Commit with amount of logic errors to fix

--- Fusion.java
+++ Fusion.java
@@ -1,28 +1,93 @@
-import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Fusion{
-    public ArrayList<Integer> list = new ArrayList<>();
+    public LinkedList<Integer> list = new LinkedList<>();
 
-    public Fusion(ArrayList<Integer> list) {
+    public Fusion(LinkedList<Integer> list) {
         this.list= list;
       }
+    
 
-      public void mergeSort(){
-        ArrayList<Integer> firstHalf = new ArrayList<>();
-        ArrayList<Integer> secondHalf = new ArrayList<>();
 
-        
-            for (int i = 0; i < list.size()-1; i+=2) {
-                firstHalf.add(list.get(i));
-                secondHalf.add(list.get(i+1));
-            }
-        
-        if (list.size() % 2 != 0) {
-            firstHalf.add(list.get(list.size()-1));
+
+    public static void divisionOfArray(LinkedList<Integer> list){
+        int listLength = list.size();
+
+        if (listLength < 2) {
+            return;
         }
-        System.out.println(firstHalf);
-        System.out.println(secondHalf);
-      }
+
+
+        int middleIndex = list.size() / 2;
+        LinkedList<Integer> firstHalf = new LinkedList<>();
+        LinkedList<Integer> secondHalf = new LinkedList<>();
+
+        for (int i = 0; i < middleIndex; i++) {
+            firstHalf.add(list.get(i));
+        }
+        for (int i = middleIndex; i < listLength; i++) {
+            secondHalf.add(list.get(i));
+        }
+
+        divisionOfArray(firstHalf);
+        divisionOfArray(secondHalf);
+
+        mergeSort(list, firstHalf, secondHalf);
+
+     }
+
+
+        public static void mergeSort(LinkedList<Integer> list,  LinkedList<Integer> firstHalf, LinkedList<Integer> secondHalf){
+            int firstSize = firstHalf.size();
+            int secondSize = secondHalf.size();
+            int i = 0, j = 0, k = 0 ;
+            while(i < firstSize && j < secondSize){
+               if (firstHalf.get(i) <= secondHalf.get(j)) {
+                    list.set(k,(firstHalf.get(i)));
+                    i++;
+               }
+               else{
+                    list.set(k,(firstHalf.get(j)));
+                    j++;
+               }
+               k++;
+            }
+            
+            while(i < firstSize){
+                list.set(k, firstHalf.get(i));
+                i++;
+                k++;
+            }
+
+            while(j < firstSize){
+                list.set(k,secondHalf.get(j));
+                j++;
+                k++;
+            }
+        }
+
+
+
+
+    // public void mergeSort(){
+    //     LinkedList<Integer> firstHalf = new LinkedList<>();
+    //     LinkedList<Integer> secondHalf = new LinkedList<>();
+    //     divisionOfArray(list, firstHalf, secondHalf);
+
+    //     for (int i = 0; i < .length; i++) {
+            
+    //     }
+    // }
 }
 
 
+
+
+    //     for (int i = 0; i < list.size()-1; i+=2) {
+    //         firstHalf.add(list.get(i));
+    //         secondHalf.add(list.get(i+1));
+    //     }
+    
+    //     if (list.size() % 2 != 0) {
+    //     firstHalf.add(list.get(list.size()-1));
+    //     }
--- Main.java
+++ Main.java
@@ -1,16 +1,20 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Main {
   public static void main(String[] args) {
-    ArrayList<Integer> list = new ArrayList<>();
+    LinkedList<Integer> list = new LinkedList<>();
     list.add(3);
     list.add(5);
     list.add(1);
     list.add(8);
     list.add(4);
-    // list.add(4);
     Fusion list1 = new Fusion(list);
-    list1.mergeSort();
+    System.out.println(list);
+    System.out.println();
+    Fusion.divisionOfArray(list);
+    System.out.println(list);
+
     // System.out.println("Before sorting - " + list1);
     // list1.sorting();
     // System.out.println("After sotring - " + list1);

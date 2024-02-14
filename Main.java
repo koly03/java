acf26c236d8aceee050ac21086cc78554c07a741
koly03 <kol.y0363@gmail.com>
2024-02-14 16:04:32 +0100

    QuickSort finished

--- Main.java
+++ Main.java
@@ -1,15 +1,21 @@
 import java.util.Random;
 import java.util.LinkedList;
+import java.util.ArrayList;
 
 public class Main {
   public static void main(String[] args) {
 
     LinkedList<Integer> list = new LinkedList<>();
+    ArrayList<Integer> list2 = new ArrayList<>();
     Random randomNumbers = new Random();
 
     for (int i = 0; i < 10; i++) {
+      list2.add(randomNumbers.nextInt(1000));
       list.add(randomNumbers.nextInt(1000));
     }
+
+    
+    
     // class Bulle tests
     // Bulle list2 = new Bulle(list);
 
@@ -20,10 +26,20 @@ public class Main {
 
 
     // class Fusion test
-    System.out.println("Before sorting   " + list + "\n\n");
-    // System.out.println();
-    list = Fusion.mergeSort(list);
-    System.out.println("After sorting   " + list + "\n\n");
+    // System.out.println("Before sorting   " + list + "\n\n");
+    
+    // // System.out.println();
+    // list = Fusion.mergeSort(list);
+    // System.out.println("After sorting   " + list + "\n\n");
 
+
+    // class QuickSort test
+    // QuickSort list3 = new QuickSort(list2, 0, list2.size()-1); 
+    // System.out.println("Before sorting   " + list3 + "\n\n");
+    // list3.quickSorting(list2, 0, list2.size()-1);
+    // System.out.println("After sorting   " + list3 + "\n\n");
+
+
+    
   }
 }
--- QuickSort.java
+++ QuickSort.java
@@ -1,3 +1,57 @@
+import java.util.ArrayList;
+
 public class QuickSort {
-    
+    public QuickSort(ArrayList<Integer> list, int g, int d){
+        this.list = list;
+        this.g = g;
+        this.d = d;
+    }
+    ArrayList<Integer> list = new ArrayList<>();
+    int g = 0;
+    int d = list.size() - 1;
+
+    public void quickSorting(ArrayList<Integer> list, int g, int d){
+       if (g < d) {
+            int a = g;
+            int b = d;
+            int pivot = list.get((a+b)/2);
+            while (a <= b) {
+                while (list.get(a) < pivot) {
+                    a += 1;
+                }
+                while (list.get(b) > pivot) {
+                    b -= 1;
+                }
+                if (a <= b) {
+                    permuter(list, a, b);
+                    a += 1;
+                    b -= 1;
+                }
+            }
+            quickSorting(list, g, b);
+            quickSorting(list, a, d);
+       }
+    }
+
+
+
+
+    public void permuter(ArrayList<Integer> list, int element1, int element2) {
+        Integer x = list.get(element1);
+        list.set(element1, list.get(element2));
+        list.set(element2, x);
+      } 
+
+
+     public String toString() {
+        StringBuilder str = new StringBuilder("[");
+        for (int i = 0; i < list.size(); i++) {
+          str.append(list.get(i));
+          if (i < list.size() - 1) {
+            str.append(", ");
+          }
+        }
+        str.append("]");
+        return str.toString();
+      }
 }

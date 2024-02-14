2b0505184d7cf892be07389f4fc6c0d07d9d8cb2
koly03 <kol.y0363@gmail.com>
2024-02-14 16:04:18 +0100

    QuickSort finished

--- Main.java
+++ Main.java
@@ -7,16 +7,16 @@ public class Main {
     LinkedList<Integer> list = new LinkedList<>();
     Random randomNumbers = new Random();
 
-    for (int i = 0; i < 1000000; i++) {
+    for (int i = 0; i < 10; i++) {
       list.add(randomNumbers.nextInt(1000));
     }
-    /*  class Bulle tests
+    // class Bulle tests
     // Bulle list2 = new Bulle(list);
 
     // System.out.println("Before sorting   " + list2 + "\n\n");
     // list2.bulleSorting();
     // System.out.println("After sorting    " + list2 + "\n\n");
-    */
+    
 
 
     // class Fusion test
